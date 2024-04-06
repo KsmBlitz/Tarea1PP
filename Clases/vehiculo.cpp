cfbe64ccff2ee36701fd22449c7796e10fcb718c
Klezya <benjamin.nunez.beat@gmail.com>
2024-04-06 10:44:18 -0300

    Inicio Clases
    a

---
+++ Clases/vehiculo.cpp
@@ -0,0 +1,18 @@
+#include "string"
+
+class vehiculo{
+    private:
+        int ruedas;
+        std::string marca;
+        bool remoto;
+        int fecha_fabriacion;
+        int valor_vehiculo;
+        int valor_articulos = 0;
+
+    public:
+
+        vehiculo(int ruedas,std::string marca,int fecha_fabriacion, int valor_vehiculos, int valor_articulos){
+
+        }
+
+};
---
+++ main.cpp
@@ -0,0 +1,1 @@
+#include "Clases/master.cpp"
